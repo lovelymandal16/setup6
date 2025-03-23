4261c8a51de47a23266733fbc123289748c33302
Lovely Mandal <155715221+lovelymandal16@users.noreply.github.com>
2025-03-23 22:48:07 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -358,7 +358,9 @@ export function attachEventListners(main) {
    const meta = document.createElement('meta');
    meta.name = 'urn:adobe:aue:config:extensions';
 
-   meta.content = 'https://localhost.corp.adobe.com:8013/resources/universal_editor.html?livecycle-forms-spa_version=local';
+  // meta.content = 'https://localhost.corp.adobe.com:8013/resources/universal_editor.html?livecycle-forms-spa_version=local'; 
+meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-530-51438302f0972ee340e528ce2ff3fac4b1bef201';
+   //PR-530-51438302f0972ee340e528ce2ff3fac4b1bef201
 
   console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
    document.head.appendChild(meta);
