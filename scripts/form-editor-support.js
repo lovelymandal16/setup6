c1054cb31200b6b8e21c258bb499f7a65eb575ff
Lovely Mandal <155715221+lovelymandal16@users.noreply.github.com>
2025-06-11 11:21:36 +0530

    Update form-editor-support.js

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -359,10 +359,10 @@ export function attachEventListners(main) {
    meta.name = 'urn:adobe:aue:config:extensions';
 
 if (window.location.href.includes('cmstg')==true){
-  meta.content =   'https://experience-stage.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-df1acaeceec9d222e87400bec649d28ea50672cc';
+  meta.content =   'https://experience-stage.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-651817707d2f106226102da70f81c86031bc8451';
 }
 else{
-  meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-df1acaeceec9d222e87400bec649d28ea50672cc';
+  meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-556-651817707d2f106226102da70f81c86031bc8451';
 }
 
   console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
